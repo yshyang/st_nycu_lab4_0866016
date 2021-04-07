1e7d17bff5c4e9aa8abf6ffe7fc71ac2166595ef
yshyang <40087754+yshyang@users.noreply.github.com>
2021-04-07 23:53:25 +0800

    Update GameTest.java

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,19 +1,10 @@
+package main.java;
 
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.rules.ExpectedException;
 
 import javax.lang.model.type.NullType;
 import java.util.PriorityQueue;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameTest {
 
